ec4178b8c5b9ffafae6464dd3cc2f60608a9e872
Kyle_Tecson <kyltecs@gmail.com>
2018-11-13 23:34:48 -0800

    add sidebar and posts to journal

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -13,15 +13,16 @@
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div>
+
 				<div class="contact-hours">
 					<div class="info">
 						<ul>
 							<li class="footer-cats">
 							CONTACT INFO
 							</li>
-							<li><a href="" class="foota">info@inhabitent.com</a></li>
-							<li><a href="" class="foota">778-456-7891</a></li>
-							<li><i class="fab fa-facebook-square"></i> <i class="fab fa-twitter-square"></i> <i class="fab fa-google-plus-g"></i></li>
+							<li><i class="fas fa-envelope"></i><a href="" class="foota">   info@inhabitent.com</a></li>
+							<li><i class="fas fa-phone" style="color:white; transform:scaleX(-1)"><a href="" class="foota"></i>   778-456-7891</a></li>
+							<li><i class="fab fa-facebook-square"></i>"   "<i class="fab fa-twitter-square"></i>"   "<i class="fab fa-google-plus-g"></i></li>
 						</ul>
 					</div>
 					<div class="hours">
@@ -31,13 +32,17 @@
 						<li>Saturday: 10am to 2pm</li>
 						<li>Sunday: Closed</li>
 					</div>
-				</div>
-
-				<div class="footer-img"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-text-dark.svg" alt=""></div>
+				
 
 				<div class="copyright">
 					<p>COPYWRITE &copy 2019 INHABITENT</p>
 				</div>
+
+					<div class="footer-img"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-text.svg" alt=""></div>
+				
+				
+				</div>
+
 
 
 			</footer><!-- #colophon -->
